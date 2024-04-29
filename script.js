55790c4978bb939cc9492507bd771022bcd6cba6
lzhansh <lzhanshayeva@nu.edu.kz>
2024-04-28 22:08:08 -0700

    Palindrome Checker

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputText = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const resultText = document.getElementById("result");
+
+function isPalindrome (str) {
+  
+  if (str === str.reverse)
+    return True;
+  else
+    return False;
+}
