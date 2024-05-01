99c3669e0b72cc55af72f515c4406cc907bbd343
lzhansh <lzhanshayeva@nu.edu.kz>
2024-05-01 15:15:58 -0700

    Palindrome Checker

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const inputText = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
 
-function isPalindrome (str) {
-  
-  if (str === str.reverse)
-    return True;
-  else
-    return False;
+function isPalindrome (stri) {
+  const cleanStr = String(stri).toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  const reverseStr = cleanStr.split('').reverse().join('');
+  return cleanStr === reverseStr;
 }
+
+function checkString () {
+  const str = inputText.value;
+  if (str.length == 0) {
+    alert("Please input a value");
+    return;
+  } 
+  
+  const isPalindromeRes = isPalindrome(str);
+  
+  if ( isPalindromeRes === true ) {
+    resultText.textContent = `${str} is a palindrome`;
+  } else {
+    resultText.textContent = `${str} is not a palindrome`;
+  }
+}
+
+checkButton.addEventListener('click' , checkString);
+
+console.log(inputText);
